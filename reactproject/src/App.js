f0776f36a8f657733d1fbc2101cb21cd655bd7d2
ankit <ankit.joshi407@gmail.com>
2019-04-10 22:26:10 +0530

    Created Skeleton of Home Page

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -5,8 +5,15 @@ class App extends Component {
   render() {
     return (
       <div>
+      <div className="header">
         Phone Directory
       </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div>
+    </div>
     );
   }
 }
